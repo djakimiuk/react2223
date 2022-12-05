1374a0e3795b0323c91c3ac76bd295cbe8bcd03e
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-12-05 10:11:35 +0100

    ps2-zad2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import logo from '../.././logo.svg';
 import styles from './Header.module.scss';
 
 class Header extends React.Component {
-    render() {
-        return (
-            <div className={styles.HeaderWrapper}>
-                Missing clock :(
-            </div>
-          );
-    }
+  render() {
+    return <div className={styles.HeaderWrapper}>{this.props.timerValue}</div>;
   }
+}
 
-  export default Header;
+export default Header;

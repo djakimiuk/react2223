0ca4d3e50e8d216c218a6fca2c95bdd3a2411f2b
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-12-10 12:41:06 +0100

    ps3-zad3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import LeftColumn from "./components/LeftColumn/LeftColumn";
-import RightColumn from "./components/RightColumn/RightColumn";
-import Header from "./components/Header/Header";
-import styles from "./App.module.scss";
-import { useState } from "react";
+import LeftColumn from './components/LeftColumn/LeftColumn';
+import RightColumn from './components/RightColumn/RightColumn';
+import Header from './components/Header/Header';
+import styles from './App.module.scss';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [timerValue, setTimerValue] = useState(0);
 
-  setTimeout(() => {
-    setTimerValue(timerValue + 1);
-  }, 1000);
+  // setTimeout(() => {
+  //   setTimerValue(timerValue + 1);
+  // }, 1000);
+
+  useEffect(() => {
+    let timer;
+    timer = setTimeout(() => {
+      setTimerValue(timerValue + 1);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timerValue]);
+
+  const resetTimer = () => {
+    setTimerValue(0);
+  };
 
   return (
     <div className={styles.appWrapper}>
       <Header timerValue={timerValue} />
       <div className={styles.columnsWrapper}>
         <LeftColumn />
-        <RightColumn />
+        <RightColumn onDoubleButtonClick={resetTimer} />
       </div>
     </div>
   );
--- src/components/RightColumn/RightColumn.js
+++ src/components/RightColumn/RightColumn.js
@@ -1,9 +1,16 @@
-import commonColumnsStyles from "../../common/styles/Columns.module.scss";
+import commonColumnsStyles from '../../common/styles/Columns.module.scss';
 
-function RightColumn() {
+function RightColumn(props) {
+  const buttonText = 'Double click should reset a timer';
+
+  const handleDoubleClick = () => {
+    props.onDoubleButtonClick();
+  };
+
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
+        <button onDoubleClick={handleDoubleClick}> {buttonText} </button>
         <p>Right column</p>
       </header>
     </div>

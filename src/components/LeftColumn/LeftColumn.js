3cd0a367e08dd9e5983c411b7636ab25b843a595
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-12-10 12:34:25 +0100

    ps3-zad1

--- src/components/LeftColumn/LeftColumn.js
+++ src/components/LeftColumn/LeftColumn.js
@@ -1,12 +1,27 @@
-import React from "react";
-import commonColumnsStyles from "../../common/styles/Columns.module.scss";
+import React from 'react';
+import commonColumnsStyles from '../../common/styles/Columns.module.scss';
 
 class LeftColumn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+    this.handleIncreaseCounter = this.handleIncreaseCounter.bind(this);
+  }
+
+  handleIncreaseCounter() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
   render() {
+    const { counter } = this.state;
     return (
       <div className={commonColumnsStyles.App}>
         <header className={commonColumnsStyles.AppHeader}>
-          <p>Left column</p>
+          <button onClick={this.handleIncreaseCounter}> INCREASE BY 1 </button>
+          <p>{counter}</p>
         </header>
       </div>
     );

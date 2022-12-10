38a2166513c3149d9b45003862d21c3404e78a6b
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-12-10 12:54:45 +0100

    ps3-zad7

--- src/components/RightColumn/RightColumn.js
+++ src/components/RightColumn/RightColumn.js
@@ -1,9 +1,13 @@
-import commonColumnsStyles from '../../common/styles/Columns.module.scss';
 import { useState } from 'react';
+// import styles from './RightColumn.module.scss';
+import commonColumnsStyles from '../../common/styles/Columns.module.scss';
 
 function RightColumn(props) {
-  const buttonText = 'Double click should reset a timer';
   const [currentInputValue, setCurrentInputValue] = useState('');
+  const [name, setName] = useState('');
+  const [zodiac, setZodiac] = useState('');
+  const [messageText, setMessageText] = useState('');
+  const buttonText = 'Double click should reset a timer';
 
   const handleDoubleClick = () => {
     props.onDoubleButtonClick();
@@ -14,14 +18,27 @@ function RightColumn(props) {
   };
 
   const handleSetTimerFromValue = () => {
-    props.setTimerFromValue(currentInputValue);
+    props.onSetTimerToValue(currentInputValue);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setMessageText(`Cześć ${name}, twój znak zodiaku to: ${zodiac}`);
+  };
+
+  const handleChange = (event) => {
+    if (event.target.name === 'name') {
+      setName(event.target.value);
+    }
+    if (event.target.name === 'zodiac') {
+      setZodiac(event.target.value);
+    }
   };
 
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
         <button onDoubleClick={handleDoubleClick}> {buttonText} </button>
-        <p>Right column</p>
         <input
           type="number"
           onChange={handleChangeInputValue}
@@ -30,6 +47,28 @@ function RightColumn(props) {
         <button onClick={handleSetTimerFromValue}>
           Set value from input to timer
         </button>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Imię:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Znak zodiaku:
+            <input
+              type="text"
+              name="zodiac"
+              value={zodiac}
+              onChange={handleChange}
+            />
+          </label>
+          <input type="submit" value="Wyślij" />
+        </form>
+        <p>{messageText}</p>
       </header>
     </div>
   );

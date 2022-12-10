455081a189198414c7f58e26c5ab90ff304d57f3
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2022-12-10 12:37:07 +0100

    ps3-zad2

--- src/components/LeftColumn/LeftColumn.js
+++ src/components/LeftColumn/LeftColumn.js
@@ -8,6 +8,7 @@ class LeftColumn extends React.Component {
       counter: 0,
     };
     this.handleIncreaseCounter = this.handleIncreaseCounter.bind(this);
+    this.handleDecreaseCounter = this.handleDecreaseCounter.bind(this);
   }
 
   handleIncreaseCounter() {
@@ -15,12 +16,25 @@ class LeftColumn extends React.Component {
       counter: this.state.counter + 1,
     });
   }
+
+  handleDecreaseCounter(event) {
+    event.preventDefault();
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  }
+
   render() {
     const { counter } = this.state;
     return (
       <div className={commonColumnsStyles.App}>
         <header className={commonColumnsStyles.AppHeader}>
-          <button onClick={this.handleIncreaseCounter}> INCREASE BY 1 </button>
+          <button
+            onClick={this.handleIncreaseCounter}
+            onContextMenu={this.handleDecreaseCounter}
+          >
+            Left click + 1 / Right click -1
+          </button>
           <p>{counter}</p>
         </header>
       </div>

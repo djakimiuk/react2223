d7b3fae2ce1d08ed90fed66a06cdad24043cc6c3
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-01-15 10:00:59 +0100

    ps4-zad1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Results from "./components/Results/Results";
+import Results from './components/Results/Results';
 // import RightColumn from './components/RightColumn/RightColumn';
-import Header from "./components/Header/Header";
-import styles from "./App.module.scss";
+import vehicles from './common/consts/vehicles';
+import Header from './components/Header/Header';
+import styles from './App.module.scss';
 
 function App() {
+  console.log('wszystkie pojazdy', vehicles);
   return (
     <div className={styles.appWrapper}>
       <Header />

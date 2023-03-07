826aaaf86b1c460fa52d41cb1010cca4fc62b7d2
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-03-07 09:25:57 +0100

    ps6-zad8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Results from "./components/Results/Results";
+import Results from './components/Results/Results';
 // import RightColumn from './components/RightColumn/RightColumn';
-import Header from "./components/Header/Header";
-import styles from "./App.module.scss";
+import Header from './components/Header/Header';
+import styles from './App.module.scss';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.loggedUser) {
+      navigate('/');
+    }
+  });
   return (
     <div className={styles.appWrapper}>
       <Header />

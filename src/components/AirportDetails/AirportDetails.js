1fb7b3a0b59e3c6450a8effad41cff673e9cbcdc
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-03-24 19:15:51 +0100

    ps7-zad4

--- src/components/AirportDetails/AirportDetails.js
+++ src/components/AirportDetails/AirportDetails.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import commonColumnsStyles from '../../common/styles/Columns.module.scss';
-
+import { useParams } from 'react-router-dom';
 const AirportDetails = () => {
-  return <div>AirportDetails</div>;
+  const params = useParams();
+  const airportsList = JSON.parse(localStorage.airports);
+  const matchAirport = airportsList.find(
+    (airport) => airport.airportId === params.id
+  );
+  return (
+    <div className={commonColumnsStyles.App}>
+      <header className={commonColumnsStyles.AppHeader}>
+        <p>Airport Details</p>
+        <span>Nazwa: {matchAirport.name}</span>
+        <span>Państwo: {matchAirport.country}</span>
+        <span>Miasto: {matchAirport.city}</span>
+        ...
+      </header>
+    </div>
+  );
 };
 
 export default AirportDetails;
--- src/components/AirportsList/AirportsList.js
+++ src/components/AirportsList/AirportsList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import commonColumnsStyles from '../../common/styles/Columns.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const AirportList = () => {
   const airportsList = JSON.parse(window.localStorage.getItem('airports'));
+  const navigate = useNavigate();
 
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
         <p>AirportList</p>
         {airportsList.map((airport) => (
-          <span>
+          <span
+            onClick={() => navigate(`/airport/details/${airport.airportId}`)}
+          >
             {' '}
             {airport.name} {airport.airportId}{' '}
           </span>

5d6c03feb592d83719bb0911d26fcfac87e25ceb
Marcin Niczyporuk <marcin.niczyporuk11@gmail.com>
2023-03-24 19:32:09 +0100

    ps7-zad5

--- src/components/AirportDetails/AirportDetails.js
+++ src/components/AirportDetails/AirportDetails.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import commonColumnsStyles from '../../common/styles/Columns.module.scss';
 import { useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 const AirportDetails = () => {
+  const navigate = useNavigate();
   const params = useParams();
   const airportsList = JSON.parse(localStorage.airports);
   const matchAirport = airportsList.find(
@@ -10,6 +14,7 @@ const AirportDetails = () => {
   return (
     <div className={commonColumnsStyles.App}>
       <header className={commonColumnsStyles.AppHeader}>
+        <ArrowBackIcon onClick={() => navigate(-1)} />
         <p>Airport Details</p>
         <span>Nazwa: {matchAirport.name}</span>
         <span>Państwo: {matchAirport.country}</span>
